15711f7252abefeae5ec4f36d4b396166ecb4700
ewbriao1978 <eduardo.briao@gmail.com>
2026-05-08 09:41:26 -0300

    teste

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,10 +1,10 @@
-const user = {
+ let userdata = {
   name: 'first',
   user: 'example',
   password: '12345'
 }
 
-const dataSalario = {
+ var dataSalario = {
   salario: 1000
 }
 
@@ -22,23 +22,23 @@ async function getSalarioLiquido(req, res) {
 }
 
 
-async function saveSalario(req, res) {
+ function saveSalario(req, res) {
     const   { salario }= req.body;
     dataSalario.salario = salario; // Atualiza o salário com o valor recebido no corpo da requisição
     return res.status(200).json({ message: 'Salário atualizado com sucesso', salario: dataSalario.salario });
 }
 
 async function getUser(req, res) {
-    return res.status(200).json(user);
+    return res.status(200).json(userdata);
 }
 
 
 async function saveUser(req,res) {
     const { name, user, password } = req.body;
-    user.name = name;
-    user.user = user;
-    user.password = password;
-    return res.status(200).json({ message: 'Usuário atualizado com sucesso', user });
+    userdata.name = name;
+    userdata.user = user;
+    userdata.password = password;
+    return res.status(200).json({ message: 'Usuário atualizado com sucesso', user: userdata });
 }
 
 module.exports = { 
